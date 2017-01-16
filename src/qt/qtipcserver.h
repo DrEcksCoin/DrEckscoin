bf304bc6fad1d2b08d8ea460db7273fb4171210a
Billy Everyteen <your_email@example.com>
2017-01-16 23:58:43 +0100

    Lets go

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define DrEcksCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "DrEcksCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
